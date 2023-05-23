09a21c6c2218d3ead3a58ccc33c856d6407ba113
EinarLop <einarlop0@gmail.com>
2023-05-23 00:01:21 -0600

    get from firestore with uid

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,37 +1,59 @@
-import { View, Text, StatusBar } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {View, Text, StatusBar} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import ArScene from '../screens/ARScene';
 import useAuth from '../hooks/useAuth';
-import { themeColors } from '../theme';
+import {themeColors} from '../theme';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
-  const {user} = useAuth()
-  if (user){
-    return(
-    <NavigationContainer>
-      <StatusBar backgroundColor={themeColors.bg}/>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} initialParams={{user : user}}/>
-        <Stack.Screen name="ARScene" options={{headerShown: false}} component={ArScene} />
-      </Stack.Navigator>
-    </NavigationContainer>)
+  const {user} = useAuth();
+  if (user) {
+    return (
+      <NavigationContainer>
+        <StatusBar backgroundColor={themeColors.bg} />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            options={{headerShown: false}}
+            component={HomeScreen}
+            initialParams={{user: user}}
+          />
+          <Stack.Screen
+            name="ARScene"
+            options={{headerShown: false}}
+            component={ArScene}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
   }
   return (
     <NavigationContainer>
-      <StatusBar backgroundColor={themeColors.bg}/>
-      <Stack.Navigator initialRouteName='Welcome'>
-        <Stack.Screen name="Welcome" options={{headerShown: false}} component={WelcomeScreen} />
-        <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
-        <Stack.Screen name="SignUp" options={{headerShown: false}} component={SignUpScreen} />
+      <StatusBar backgroundColor={themeColors.bg} />
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          options={{headerShown: false}}
+          component={WelcomeScreen}
+        />
+        <Stack.Screen
+          name="Login"
+          options={{headerShown: false}}
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="SignUp"
+          options={{headerShown: false}}
+          component={SignUpScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,141 +1,160 @@
-import { View, Text, Image, TouchableOpacity, StyleSheet , Alert} from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { themeColors } from '../theme'
-import { useNavigation } from '@react-navigation/native'
-import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin'
-import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {themeColors} from '../theme';
+import {useNavigation} from '@react-navigation/native';
+import {
+  GoogleSignin,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import {GoogleAuthProvider, signInWithCredential} from 'firebase/auth';
 
 GoogleSignin.configure({
-    webClientId: "804315558824-82fsnnqs3gevbdq8naitec859qdeqjs5.apps.googleusercontent.com",
-    offlineAccess : true,
-})
+  webClientId:
+    '804315558824-82fsnnqs3gevbdq8naitec859qdeqjs5.apps.googleusercontent.com',
+  offlineAccess: true,
+});
 
 export default function WelcomeScreen() {
-    const navigation = useNavigation();
-    const signInGoogle = async () => {
-        try {
-            await GoogleSignin.hasPlayServices();
-            const { idToken } = await GoogleSignin.signIn();
-            const googleCredentials = GoogleAuthProvider.credential(idToken)
-            await signInWithCredential(auth, googleCredentials)
-        } catch (error) {
-            const msg = error.message
-            console.log("Sign In With Google Error : ", msg)
-            Alert.alert("Sign In With Google Error : ", msg)
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                // user cancelled the login flow
-            } else if (error.code === statusCodes.IN_PROGRESS) { // operation (e.g. sign in) is in progress already
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                // play services not available or outdated
-            } else {
-            }
-
-        }
+  const navigation = useNavigation();
+  const signInGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const {idToken} = await GoogleSignin.signIn();
+      const googleCredentials = GoogleAuthProvider.credential(idToken);
+      await signInWithCredential(auth, googleCredentials);
+    } catch (error) {
+      const msg = error.message;
+      console.log('Sign In With Google Error : ', msg);
+      Alert.alert('Sign In With Google Error : ', msg);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+      }
     }
-    return (
-        <SafeAreaView style={[styles.container, { backgroundColor: themeColors.bg }]}>
-            <View style={[styles.containerX]}>
-                <Text style={styles.text}>Tech People</Text>
-                <View style={styles.imgContainer}>
-                    <Image
-                        source={require('../assets/images/claroS.png')}
-                        style={styles.image}
-                    />
-                </View>
-                <View >
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate('SignUp')}
-                        style={styles.button}
-                    >
-                        <Text style={[styles.buttonText]}>
-                            Sign Up
-                        </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => signInGoogle()} style={[styles.button, { backgroundColor: themeColors.crema }]}>
-                        <View style={{
-                            marginHorizontal: 12,
-                            flexDirection: "row",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}>
-                            <Image source={require('../assets/images/google.png')} style={{
-                                width: 28,
-                                height: 28,
-                                marginRight: 8,
-                            }} />
-                            <Text style={[styles.buttonText]}>Sign In with Google</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <View style={styles.link}>
-                        <Text style={[styles.textWhite, styles.fontSemibold]}>Already have an account?</Text>
-                        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                            <Text style={[styles.fontSemibold, styles.textYellow400]}> Log In</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
+  };
+  return (
+    <SafeAreaView style={[styles.container, {backgroundColor: themeColors.bg}]}>
+      <View style={[styles.containerX]}>
+        <Text style={styles.text}>Tech People</Text>
+        <View style={styles.imgContainer}>
+          <Image
+            source={require('../assets/images/claroS.png')}
+            style={styles.image}
+          />
+        </View>
+        <View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('SignUp')}
+            style={styles.button}>
+            <Text style={[styles.buttonText]}>Sign Up</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => signInGoogle()}
+            style={[styles.button, {backgroundColor: themeColors.crema}]}>
+            <View
+              style={{
+                marginHorizontal: 12,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Image
+                source={require('../assets/images/google.png')}
+                style={{
+                  width: 28,
+                  height: 28,
+                  marginRight: 8,
+                }}
+              />
+              <Text style={[styles.buttonText]}>Sign In with Google</Text>
             </View>
-        </SafeAreaView>
-    );
+          </TouchableOpacity>
+          <View style={styles.link}>
+            <Text style={[styles.textWhite, styles.fontSemibold]}>
+              Already have an account?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+              <Text style={[styles.fontSemibold, styles.textYellow400]}>
+                {' '}
+                Log In
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    containerX: {
-        flex: 1,
-        backgroundColor: themeColors.bg,
-        marginVertical: 16,
-        justifyContent: "space-around"
-    },
-    text: {
-        color: themeColors.crema,
-        fontWeight: 'bold',
-        fontSize: 40,
-        textAlign: 'center',
-    },
-    imgContainer: {
-        flexDirection: "row",
-        justifyContent: "center"
-    },
-    image: {
-        width: 250,
-        height: 250,
-    },
-    button: {
-        paddingVertical: 12,
-        marginHorizontal: 32,
-        marginVertical: 12,
-        backgroundColor: themeColors.accent,
-        borderRadius: 12,
-    },
-    buttonText: {
-        color: themeColors.darkGray,
-        fontSize: 18,
-        lineHeight: 28,
-        fontWeight: "700",
-        textAlign: "center",
-    },
-    linkText: {
-        color: 'white',
-        fontWeight: 'bold',
-    },
-    link: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: 16,
-    },
-    spaceY4: {
-        marginTop: 16
-    },
-    textWhite: {
-        color: "white"
-    },
-    fontSemibold: {
-        fontWeight: "700"
-    },
-    textYellow400: {
-        color: themeColors.accent,
-    }
+  container: {
+    flex: 1,
+  },
+  containerX: {
+    flex: 1,
+    backgroundColor: themeColors.bg,
+    marginVertical: 16,
+    justifyContent: 'space-around',
+  },
+  text: {
+    color: themeColors.crema,
+    fontWeight: 'bold',
+    fontSize: 40,
+    textAlign: 'center',
+  },
+  imgContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 250,
+    height: 250,
+  },
+  button: {
+    paddingVertical: 12,
+    marginHorizontal: 32,
+    marginVertical: 12,
+    backgroundColor: themeColors.accent,
+    borderRadius: 12,
+  },
+  buttonText: {
+    color: themeColors.darkGray,
+    fontSize: 18,
+    lineHeight: 28,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  linkText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  link: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 16,
+  },
+  spaceY4: {
+    marginTop: 16,
+  },
+  textWhite: {
+    color: 'white',
+  },
+  fontSemibold: {
+    fontWeight: '700',
+  },
+  textYellow400: {
+    color: themeColors.accent,
+  },
 });
